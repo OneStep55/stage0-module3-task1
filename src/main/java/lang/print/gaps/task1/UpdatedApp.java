157299dbc1e2293cb18f5caf7e546b76a059e7f0
OneStep55 <vnmcfn@gmail.com>
2022-07-27 19:33:37 +0600

    Complete tasks

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,6 +2,15 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("I'm\nupdated\ncan\nbe\ncompiled\nAnd\ncan\nwrite\nto\nconsole");
+        System.out.println("I'm");
+        System.out.println("updated");
+        System.out.println("can");
+        System.out.println("be");
+        System.out.println("compiled.");
+        System.out.println("And");
+        System.out.println("can");
+        System.out.println("write");
+        System.out.println("to");
+        System.out.println("console");
     }
 }
